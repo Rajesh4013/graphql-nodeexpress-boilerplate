781212a32a5669cac3a1683bf894b5214e2d435d
Rajesh.K <rajesh_kandregula@tecnics.com>
2024-12-02 18:08:57 +0530

    Setup connections
    Set up Prisma Client with database connection using environment variables.

---
+++ connection.js
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@prisma/client";
+import config from "./config.js";
+
+const prismaConnection = new PrismaClient({
+  datasources: {
+    db: {
+      url: `postgresql://${config.db.user}:${config.db.pass}@${config.db.host}/${config.db.name}?connection_limit=${config.db.connectionLimit}`,
+    },
+  },
+  log: ["info"],
+});
+
+export { prismaConnection };
