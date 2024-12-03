cca60d8066c1db0609d43cef488bda046bd4b366
Abhi Kumar <abhi_yallapu@tecnics.com>
2024-12-03 17:38:36 +0530

    Refactor media repos to use raw SQL query
    - Replaced Prisma's findMany with raw SQL using $queryRaw for greater flexibility.
    
    Updated fetchMediaItems, fetchMediaItemsById, and fetchDynamicMediaItems functions.

--- repos/repos.media.js
+++ repos/repos.media.js
@@ -2,19 +2,19 @@ import { prismaConnection as prisma } from "../connection.js";
 
 export async function fetchMediaItems() {
   try {
-    return await prisma.media.findMany();
+    return await prisma.$queryRaw`SELECT * FROM "Media" m;`;
   } catch (err) {
-    // console.error("Error fetching media items:", err);
+    console.error("Error fetching media items:", err);
     return [];
   }
 }
 
 export async function fetchMediaItemById(mediaId) {
   try {
-    const media = await prisma.media.findUnique({ where: { mediaId } });
-    return media;
+    const media: any = await prisma.$queryRaw`SELECT * FROM "Media" m WHERE m."mediaId" = ${mediaId}`;
+    return media? media[0]: null;
   } catch (err) {
-    // console.error("Error fetching media item by ID:", err);
+    console.error("Error fetching media item by ID:", err);
     return null;
   }
 }
@@ -29,64 +29,44 @@ export async function fetchDynamicMediaItems(playlistConfig) {
   } = playlistConfig;
 
   try {
-    const conditions = [];
+    let query = `SELECT * FROM "Media" m WHERE 1=1`;
 
     if (tags?.include) {
-      conditions.push({
-        tags: {
-          contains: tags.include,
-        },
-      });
+      query += ` AND m."tags" ILIKE '%${tags.include}%'`;
     }
 
     if (tags?.exclude) {
-      conditions.push({
-        NOT: {
-          tags: {
-            contains: tags.exclude,
-          },
-        },
-      });
+      query += ` AND m."tags" NOT ILIKE '%${tags.exclude}%'`;
     }
 
     if (customParameters?.include) {
       Object.entries(customParameters.include).forEach(([key, value]) => {
-        conditions.push({
-          customParameters: {
-            path: [key],
-            equals: value,
-          },
-        });
+        query += ` AND m."customParameters" @> '{"${key}": "${value}"}'`;
       });
     }
 
     if (customParameters?.exclude) {
       Object.entries(customParameters.exclude).forEach(([key, value]) => {
-        conditions.push({
-          NOT: {
-            customParameters: {
-              path: [key],
-              equals: value,
-            },
-          },
-        });
+        query += ` AND m."customParameters" NOT @> '{"${key}": "${value}"}'`;
       });
     }
 
-    const playlistPreview = await prisma.media.findMany({
-      where: conditions.length > 0 ? { AND: conditions } : undefined,
-      orderBy: sort?.field
-        ? {
-            [sort.field]: sort.order,
-          }
-        : undefined,
-      skip: (pageNumber - 1) * itemsPerPage,
-      take: itemsPerPage,
-    });
+    if (sort?.field && sort?.order) {
+      query += ` ORDER BY m."${sort.field}" ${sort.order}`;
+    }
+
+    query += ` LIMIT ${itemsPerPage} OFFSET ${(pageNumber - 1) * itemsPerPage}`;
+
+    console.log(query);
+    const playlistPreview = await prisma.$queryRawUnsafe(query);
 
     return playlistPreview;
-  } catch (err) {
-    // console.error("Error fetching playlist preview:", err);
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      console.error("Error fetching playlist preview:", err.message);
+    } else {
+      console.error("Unknown error occurred:", err);
+    }
     return [];
   }
 }
