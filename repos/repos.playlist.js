b8f815560933406e39c538e3a180fcf37b4a12ca
Abhi Kumar Yallapu <abhi_yallapu@tecnics.com>
2024-12-09 14:47:20 +0530

    Refactor playlist repo functions to use raw SQL query
    - Replaced Prisma's create and update with raw SQL using $queryRawUnsafe for greater flexibility.
    - Updated createPlaylist, getPlaylistById, and updatePlaylist functions.

--- repos/repos.playlist.js
+++ repos/repos.playlist.js
@@ -1,21 +1,44 @@
 import { prismaConnection as prisma } from "../connection.js";
 
-export async function createPlaylist(inputPlaylist, playlist_config) {
+export async function createPlaylist(inputPlaylist, playlistConfig) {
   try {
-    const { title, feedid, type, description, playlist, ...customParameters } =
-      inputPlaylist;
-    const playlistData = await prisma.playlist.create({
-      data: {
-        playlist_id: feedid,
-        title,
-        type,
-        description,
-        playlist: playlist,
-        custom_parameters: customParameters,
-        playlist_config,
-      },
+    const {
+      title,
+      feedid,
+      type,
+      description,
+      playlist,
+      ...customParameters
+    } = inputPlaylist;
+
+    const playlistJson = playlist.map((playlistItem) => {
+      return JSON.stringify(playlistItem);
     });
-    return playlistData;
+
+    const query = `
+      INSERT INTO "Playlist" (
+        "playlist_id", 
+        "title", 
+        "type", 
+        "description", 
+        "playlist", 
+        "custom_parameters", 
+        "playlist_config"
+      ) 
+      VALUES (
+        $1, 
+        $2, 
+        $3, 
+        $4, 
+        $5::jsonb[],
+        $6::jsonb, 
+        $7::jsonb
+      )
+      RETURNING *;
+    `;
+    const playlistData = await prisma.$queryRawUnsafe(query, feedid, title, type, description, playlistJson, customParameters, playlistConfig);
+
+    return playlistData[0];
   } catch (error) {
     console.log(`Error creating playlist: ${error.message}`);
     throw new Error("Failed to create playlist. Please try again later.");
@@ -24,19 +47,34 @@ export async function createPlaylist(inputPlaylist, playlist_config) {
 
 export async function updatePlaylist(playlistId, updatedData) {
   try {
-    const { title, kind, description, playlist, ...customParameters } =
-      updatedData;
-    const updatedPlaylist = await prisma.playlist.update({
-      where: { playlistId },
-      data: {
-        title,
-        type: kind,
-        description,
-        playlist,
-        customParameters,
-      },
+    const {
+      title,
+      kind,
+      description,
+      playlist,
+      playlistConfig,
+      ...customParameters
+    } = updatedData;
+    const playlistJson = playlist.map((playlistItem) => {
+      return JSON.stringify(playlistItem);
     });
-    return updatedPlaylist;
+    const query = `
+    UPDATE "Playlist"
+    SET
+      "title" = $1,
+      "type" = $2,
+      "description" = $3,
+      "playlist" = $4::jsonb[],
+      "custom_parameters" = $5::jsonb,
+      "playlist_config" = $6::jsonb,
+      "updated_at" = NOW()
+    WHERE
+      "playlist_id" = $7
+    RETURNING "playlist_id", "title", "type", "description", "playlist", "custom_parameters";
+  `;
+
+    const updatedPlaylist = await prisma.$queryRawUnsafe(query, title, kind, description, playlistJson, customParameters, playlistConfig, playlistId);
+    return updatedPlaylist[0];
   } catch (error) {
     console.log(
       `Error updating playlist with ID ${playlistId}: ${error.message}`
@@ -47,12 +85,8 @@ export async function updatePlaylist(playlistId, updatedData) {
 
 export async function getPlaylistById(playlistId) {
   try {
-    const playlist = await prisma.playlist.findUnique({
-      where: {
-        playlist_id: playlistId,
-      },
-    });
-    return playlist;
+    const playlist = await prisma.$queryRaw`SELECT * FROM "Playlist" p WHERE p."playlist_id" = ${playlistId};`;
+    return playlist[0];
   } catch (error) {
     console.log(error);
     return null;
