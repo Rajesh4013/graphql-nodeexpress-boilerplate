fa3e853e6a1acd290055bfaeed6589b6f1e7780d
Deepika2430 <deepika_pabbisetti@tecnics.com>
2024-12-10 10:58:48 +0530

    Refactor playlist creation and updation
    - Merged `custom_parameters` into media items.
    - Updated `playlist_id` to use full UUID.
    - Added `custom_parameters` to create and update functions.
    - Improved handling of `dynamicPlaylistConfig` during update.

--- resolvers/resolvers.playlist.js
+++ resolvers/resolvers.playlist.js
@@ -1,21 +1,23 @@
 import { v4 as uuidv4 } from "uuid";
 
-import { formatMedia } from "../utils/utils.media.js";
 import * as repos from "../repos/repos.playlist.js";
 import { fetchDynamicMediaItems } from "../repos/repos.media.js";
 import { formatPlaylist } from "../utils/utils.playlist.js";
 
 export async function createPlaylist(playlistMetadata, dynamicPlaylistConfig) {
   const mediaItems = await fetchDynamicMediaItems(dynamicPlaylistConfig);
-  console.log(mediaItems.length);
   const convertedMediaItems = await Promise.all(
-    mediaItems.map((mediaItem) => formatMedia(mediaItem))
+    mediaItems.map((mediaItem) => {
+      let { custom_parameters, ...media } = mediaItem;
+      return { ...media, ...custom_parameters };
+    })
   );
 
-  const playlist_id = uuidv4().substr(0, 6);
+  const playlist_id = uuidv4();
   const playlist = formatPlaylist({
     playlist_id,
     playlist: convertedMediaItems,
+    custom_parameters: playlistMetadata.customParameters,
     ...playlistMetadata,
   });
 
@@ -30,17 +32,22 @@ export async function updatePlaylist(
 ) {
   const mediaItems = await fetchDynamicMediaItems(dynamicPlaylistConfig);
   const convertedMediaItems = await Promise.all(
-    mediaItems.map((mediaItem) => formatMedia(mediaItem))
+    mediaItems.map((mediaItem) => {
+      let { custom_parameters, ...media } = mediaItem;
+      const updatedMediaItem = { ...media, ...custom_parameters };
+      return updatedMediaItem;
+    })
   );
 
   const updatedPlaylist = formatPlaylist({
     playlistId,
     playlist: convertedMediaItems,
+    custom_parameters: playlistMetadata.customParameters,
     ...playlistMetadata,
   });
 
   return formatPlaylist(
-    await repos.updatePlaylist(playlistId, updatedPlaylist)
+    await repos.updatePlaylist(playlistId, { playlistConfig: dynamicPlaylistConfig, ...updatedPlaylist })
   );
 }
 
