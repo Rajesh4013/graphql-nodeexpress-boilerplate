99f5f84842dede359215102adf301bfe95972581
Rajesh.K <rajesh_kandregula@tecnics.com>
2024-12-12 14:38:25 +0530

    Refactor playlist resolvers
    - Modified playlist resolvers to send media_id instead of playlist object to repo functions and util functions.

--- resolvers/resolvers.playlist.js
+++ resolvers/resolvers.playlist.js
@@ -1,22 +1,15 @@
 import { v4 as uuidv4 } from 'uuid';
 
 import * as repos from '../repos/repos.playlist.js';
-import { fetchDynamicMediaItems } from '../repos/repos.media.js';
+import { fetchDynamicMediaIds } from '../repos/repos.media.js';
 import { formatPlaylist } from '../utils/utils.playlist.js';
 
 export async function createPlaylist(playlistMetadata, dynamicPlaylistConfig) {
-  const mediaItems = await fetchDynamicMediaItems(dynamicPlaylistConfig);
-  const convertedMediaItems = await Promise.all(
-    mediaItems.map((mediaItem) => {
-      let { custom_parameters :customParameters, ...media } = mediaItem;
-      return { ...media, ...customParameters };
-    })
-  );
-
+  const media_id = await fetchDynamicMediaIds(dynamicPlaylistConfig);
   const playlist_id = uuidv4();
   const playlist = formatPlaylist({
     playlist_id,
-    playlist: convertedMediaItems,
+    media_id,
     custom_parameters: playlistMetadata.customParameters,
     ...playlistMetadata,
   });
@@ -33,17 +26,10 @@ export async function updatePlaylist(
   playlistMetadata,
   dynamicPlaylistConfig
 ) {
-  const mediaItems = await fetchDynamicMediaItems(dynamicPlaylistConfig);
-  const convertedMediaItems = await Promise.all(
-    mediaItems.map((mediaItem) => {
-      let { custom_parameters, ...media } = mediaItem;
-      return { ...media, ...custom_parameters };
-    })
-  );
-
+  const media_id = await fetchDynamicMediaIds(dynamicPlaylistConfig);
   let updatedPlaylist = formatPlaylist({
     playlistId,
-    playlist: convertedMediaItems,
+    media_id,
     custom_parameters: playlistMetadata.customParameters,
     ...playlistMetadata,
   });
